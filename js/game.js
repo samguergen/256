48b789c7380cda234c4530e9810c9912146520b7
Ryan & Awesome Ninja Fighter <pair+rimmesbe.samguergen@devbootcamp.com>
2015-04-13 11:23:23 -0400

    added method to convert to 2D array and to_s method

---
+++ js/game.js
@@ -0,0 +1,38 @@
+// require 'underscore.js'
+
+// -create a 2D array from given string *done*
+// - add to to_s method to convert 2D array back to string
+// -generate a 2 (or 4) in one of the empty cells
+// -allow key arrows to shift cell numbers on board
+// -create method: if 2 same number are consecutive ,
+//   multiply them and shift the following numbers closer.
+// -if arrow right or left, generate method
+// -if arrow up or down, transpose board and generate method (then transpose back)
+// -generate a 2 (or 4) in one of the empty cells for every key move
+
+
+var board = function(str){
+  this.strg = str;
+  this.createBoard = function(){
+    var numbers = this.strg.split('');
+    this.game_array = [];
+    for (var row = 0; row < 4; row++) {
+      var row_array = [];
+      for (var col = 0; col < 4; col++) {
+        row_array.push((numbers[(4 * row) + col]) * 1);
+      };
+      this.game_array.push(row_array);
+    };
+    console.log(this.game_array);
+  };
+
+  this.to_s = function(){
+    var flat_arr = [].concat.apply([],this.game_array);
+    console.log(flat_arr.join(''));
+  };
+};
+
+game1 = new board('0002000000000000');
+game1.createBoard();
+// console.log(game1.game_array);
+game1.to_s();
